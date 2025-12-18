24a7e98bbb434b59ecb4cb4746ad779bfa6ab193
danhdt90 <danhdt90@propolife.co.jp>
2025-12-18 15:14:41 +0700

    hotfix: add Google Tag Manager script for tracking

--- wp-content/themes/brandwoods-2025/includes/head.php
+++ wp-content/themes/brandwoods-2025/includes/head.php
@@ -18,4 +18,14 @@
     <link rel="stylesheet" href="<?= STYLESHEET_PATH_FE; ?>/fe-styles.css">
 
     <?php wp_head(); ?>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-XXXXXXXXXX');
+    </script>
 </head>
